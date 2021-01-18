63e77765378f43236921fafbb9e7c195a0025a8d
Ukang'a Dickson <ukanga@users.noreply.github.com>
2021-01-18 20:08:52 +0300

    Interpolation-only expressions are deprecated

--- main.tf
+++ main.tf
@@ -6,10 +6,10 @@
 ###########################################################################
 
 data "aws_vpc" "default" {
-  id      = "${var.default_vpc}"
+  id      = var.default_vpc
   default = true
 }
 
 data "aws_route_table" "default" {
-  route_table_id = "${length(var.default_route_table) > 0 ? var.default_route_table : aws_vpc.main.main_route_table_id}"
+  route_table_id = length(var.default_route_table) > 0 ? var.default_route_table : aws_vpc.main.main_route_table_id
 }

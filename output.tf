eccf06f3057948ea5f413b21afd74a80b0e273e5
Ukang'a Dickson <ukanga@users.noreply.github.com>
2021-01-18 20:08:52 +0300

    Interpolation-only expressions are deprecated

--- output.tf
+++ output.tf
@@ -1,11 +1,11 @@
 output "main_vpc_id" {
-  value = "${aws_vpc.main.id}"
+  value = aws_vpc.main.id
 }
 
 output "subnet_ids" {
-  value = "${aws_subnet.onadata-api-subnets.*.id}"
+  value = aws_subnet.onadata-api-subnets.*.id
 }
 
 output "vpc_arn" {
-  value = "${aws_vpc.main.arn}"
+  value = aws_vpc.main.arn
 }
